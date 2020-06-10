1b5bb4b93580095e1882b4e70deb1d0fe919fea0
Eden Brawn <edenbrawn@gmail.com>
2020-06-10 14:05:18 +0100

    commit7

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 
 function differentpic() { /* Help with switching picture (https://stackoverflow.com/questions/29980711/javascript-hover-text-over-image)*/
     document.getElementById("me") .src = "assets/unnamed2.jpg";
-    document.getElementById("text").setAttribute("style", "display:block");
+    
   };
   function normalPic() {
     document.getElementById("me") .src = "assets/20200109_111813(2).jpg";
   };
-
+function textAppear() {
+    document.getElementById("text").setAttribute("style", "display:block");
+};
+setTimeout(textAppear, 3000); /*Help with tiemout (https://javascript.info/settimeout-setinterval)*/ 
 function setTheme(background) { /*Help with light dark button (https://zocada.com/dark-and-light-theme-switcher-using-css-variables-and-pure-javascript/)*/
     localStorage.setItem('colour', background);
     document.documentElement.className = background;
@@ -29,7 +32,6 @@ function change() {
  })();
 
 
- 
  
 
 
