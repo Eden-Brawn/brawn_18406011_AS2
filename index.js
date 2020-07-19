8c8942c84bd424d9bc3b45da8aaca76bf77817b9
Eden Brawn <edenbrawn@gmail.com>
2020-07-19 09:56:56 +0100

    commit17

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function differentpic() { /* Help with switching picture (https://stackoverflow.com/questions/29980711/javascript-hover-text-over-image)*/
    document.getElementById("me") .src = "assets/unnamed2.jpg";
    
- }
+ }/*Help from (/*Based on (https://github.com/lizcoultersmith/starter-landing/blob/master/scripts.js) */
  function normalPic() {
    document.getElementById("me") .src = "assets/20200109_111813(2).jpg";
  }
@@ -38,7 +38,7 @@ function change() {
 
 
 
-function git(){
+function git(){ /*Help with setAttribute (https://developer.mozilla.org/en-US/docs/Web/API/Element/setAttribute) */
    let popup  = document.getElementById("gitTag").setAttribute("style", "display:block");
 }
 function noGit(){
@@ -70,7 +70,7 @@ function menuChange(){
     let theButton = document.getElementById("toggle");
     let theMenu = document.querySelector(".popout");
 
-    if (menuDisplay === false) {
+    if (menuDisplay === false) { /*Help with if statements (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/if...else)*/
        theMenu.style.display = "block"; /*Help with innerHTML (https://stackoverflow.com/questions/45378428/how-to-dynamically-add-a-font-awesome-icon-with-javascript) and (https://www.quora.com/What-is-the-definition-of-innerHTML#:~:text=innerHTML%20is%20a%20DOM%20property,in%20Javascript%20to%20manipulate%20DOM.)*/
        theButton.innerHTML = '<i class="fas fa-times fa-2x"></i>';/*The source for cross icon (https://fontawesome.com/icons/times?style=solid)*/
        menuDisplay = true;
@@ -78,7 +78,7 @@ function menuChange(){
     }
     else if (menuDisplay === true)  {
        theMenu.style.display = "none";
-       theButton.innerHTML = '<i id="togpic"class="fas fa-bars fa-2x"></i>';
+       theButton.innerHTML = '<i id="togpic"class="fas fa-bars fa-2x"></i>'; /*The source for hamburger icon (https://fontawesome.com/icons/bars?style=solid) */
        menuDisplay = false;
        
     }
@@ -93,7 +93,7 @@ function menuChange(){
      window.addEventListener('scroll', () => {
     if (menuDisplay === true && window.scrollY > 50){
         ham.classList.add(l);
-        ham.classList.remove(cb);
+        ham.classList.remove(cb); /*Help with classList, add, remove (https://developer.mozilla.org/en-US/docs/Web/API/Element/classList#:~:text=classList%20is%20a%20read%2Donly,space%2Ddelimited%20string%20via%20element.) */
     }
     else{
        ham.classList.add(cb);
